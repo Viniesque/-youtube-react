23796bf146c32e6ad2b1b46176eb6b0f940a4a25
Viniesque Damiao <viniesque@outlook.com>
2020-08-15 15:50:51 -0300

    Commit de criação da barra de navegação de cabeçalho no navegador.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import HeaderNav from './containers/HeaderNav/HeaderNav';
 
 
 function App() {
   return (
-    <div>
-      YouTube em Breve ! 
-    </div>
+    <HeaderNav />
   );
 }
 
